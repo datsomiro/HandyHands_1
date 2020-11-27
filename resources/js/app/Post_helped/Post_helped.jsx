59fa3f5bd63810e6d7179db5240b50d1e57ec0a3
datsomiro <61497396+datsomiro@users.noreply.github.com>
2020-11-27 12:49:30 +0100

    helped

--- resources/js/app/Post_helped/Post_helped.jsx
+++ resources/js/app/Post_helped/Post_helped.jsx
@@ -60,50 +60,51 @@ export default function Post_helped() {
     return (
         <div className="Post_helped-box">
 
-            <form action="/helped" className="Post_helped" method="post" onSubmit={handleSubmit}>                
-                        <div className="help_label">
-                            <label htmlFor="object"></label>
-                            <select id="service_categories" name="service_categories" onChange={handleChange} value={values.service_categories} >
-                                <option value="food">Food</option>
-                                <option value="handyman">Handy Man</option>
-                                <option value="drive">A drive</option>
-                                <option value="Shopping">Shopping</option>
-                                <option value="other">Other</option>
-                            </select>
-                            <label htmlFor="when"></label>
-                            <input type="text" id="time" name="time"
-                                onChange={handleChange} placeholder="When?" value={values.time} />
-                            <label htmlFor="location"></label>
-                            <select id="location" name="location" onChange={handleChange} value={values.location}>
-                                <option name="location">Prague1</option>
-                                <option name="location">Prague2</option>
-                                <option name="location">Prague3</option>
-                                <option name="location">Prague4</option>
-                                <option name="location">Prague5</option>
-                                <option name="location">Prague6</option>
-                                <option name="location">Prague7</option>
-                                <option name="location">Prague8</option>
-                                <option name="location">Prague9</option>
-                                <option name="location">Prague10</option>
-                                <option name="location">Unetice</option>
-                                <option name="location">Suchdol</option>
-                                <option name="location">Roztoky</option>
-                                <option name="location">Troja</option>
-                                <option name="location">Radotin</option>
-                                <option name="location">Dolni Chriby</option>
-                            </select>   
-                            <label htmlFor="cost"></label>
-                            <input type="text" id="cost" placeholder="what in exchange" name="cost" onChange={handleChange} value={values.cost} /><br />
-                            <input type="hidden" id="volunteer" name="volunteer" onChange={handleChange} value="0"></input>
-                        </div>              
-                        <div className="textaera">
-                            <textarea id="textarea" cols="40" rows="5" name="description" onChange={handleChange} value={values.description} >
-                            </textarea>
-                        </div>                   
-                        <UploadPic />                   
-                        <button className="submit" type="submit" value="submit" onChange={handleChange}>SUBMIT</button>
-                    
-               
+            <form action="/helped" className="Post_helped" method="post" onSubmit={handleSubmit}>
+                <div className="help_label">
+                    <label htmlFor="object"></label>
+                    <select id="service_categories" name="service_categories" onChange={handleChange} value={values.service_categories} >
+                        <option value="food">Food</option>
+                        <option value="handyman">Handy Man</option>
+                        <option value="drive">A drive</option>
+                        <option value="Shopping">Shopping</option>
+                        <option value="other">Other</option>
+                    </select>
+                    <label htmlFor="when"></label>
+                    <input type="text" name="time" onChange={handleChange} placeholder="When?" value={values.time} />
+                    <label htmlFor="location"></label>
+                    <select id="location" name="location" onChange={handleChange} value={values.location}>
+                        <option value="Prague1">Prague1</option>
+                        <option value="Prague2">Prague2</option>
+                        <option value="Prague3">Prague3</option>
+                        <option value="Prague4">Prague4</option>
+                        <option value="Prague5">Prague5</option>
+                        <option value="Prague6">Prague6</option>
+                        <option value="Prague7">Prague7</option>
+                        <option value="Prague8">Prague8</option>
+                        <option value="Prague9">Prague9</option>
+                        <option value="Prague10">Prague10</option>
+                        <option value="Unetice">Unetice</option>
+                        <option value="Suchdol">Suchdol</option>
+                        <option value="Roztoky">Roztoky</option>
+                        <option value="Troja">Troja</option>
+                        <option value="Radotin">Radotin</option>
+                        <option value="Dolni Chriby">Dolni Chriby</option>
+                    </select>
+                    <label htmlFor="cost"></label>
+                    <input type="text" placeholder="what in exchange" name="cost" onChange={handleChange} value={values.cost} /><br />
+                    <input type="hidden" name="volunteer" onChange={handleChange} value="0"></input>
+                </div>
+                <div className="textaera">
+                    <textarea id="textarea" cols="40" rows="5" name="description" onChange={handleChange} value={values.description} >
+                    </textarea>
+                </div>
+                <div className="bottom">
+                    <UploadPic />
+                    <button className="submit" type="submit" value="submit" onChange={handleChange}>SUBMIT</button>
+                </div>
+
+
             </form>
         </div>
     )
